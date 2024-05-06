2d6d6bac0e355a9f142cb2d0d4d067ae8a9d8111
marianelarce <arcemarianel@gmail.com>
2024-05-05 22:20:31 -0400

    primer commit

---
+++ indexFormulario.hyml.js
@@ -0,0 +1,37 @@
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href=Pets.css />
+ <title> Validaciones con Javascript </title>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+ <link rel="stylesheet" href="Pets.css">
+ <script type="text/Javascript" src="js/jquery.js">
+    
+ 
+    </head>
+    <body>
+    <!--Menu de navegacion del sitio-->
+    
+<ul class="navbar">
+
+  <li> <a href="PETSHOP.htm"> NUESTROS SERVICIOS</a></li>
+   <li> <a href="ALIMENTO%20PERRO.htm"> ALIMENTO PARA PERROS</a></li>
+    <li> <a href="ALIMENTO%20GATO.htm"> ALIMENTO PARA GATOS </a> </li>
+    
+    
+</ul> 
+        <div class="img-fondo">
+        
+          <div class="velo-negro">
+              
+
+<!-------------------------------------------------------------------->
+              <!--ESTE ES LA CABECERA DE NUESTRO DOCUMENTO INDEX-->
+              <header class="header">
+                  <a href="NuevoPets.htm">
+                   <img src="CHARLY.jpg" alt="logo" class="logo-img">
+                  
+
+                  </a>
+              </header>
+               <!---FIN DEL HEADER-->
